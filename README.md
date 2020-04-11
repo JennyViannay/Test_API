84bfbeabfd231f155b294c9e168209cb66b13dc2
JennyViannay <jenny.viannay75@gmail.com>
2020-04-11 13:18:58 +0200

    spy api

--- README.md
+++ README.md
@@ -1,3 +1,2 @@
-# Here call API HEROKU DRANBON BALL Z with cURL to : https://dragon-ball-api.herokuapp.com/api/character
+# Here call API IP GEO LOCATION with cURL to : https://ip-geo-location.p.rapidapi.com/ip/37.140.128.11?format=json
 
-## API DOC : https://dragon-ball-api.herokuapp.com/documentation
--- index.php
+++ index.php
@@ -1,61 +1,53 @@
 <?php
-// 1 Initialiser CURL
-$ch = curl_init();
+//to get ip adress of client :
+// if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+//     $ip = $_SERVER['HTTP_CLIENT_IP'];
+// } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+//     $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+// } else {
+//     $ip = $_SERVER['REMOTE_ADDR'];
+// }
 
-// 2 Options :  
-// URL to send in request => GET ALL CHARACTERS
-curl_setopt($ch, CURLOPT_URL, "https://dragon-ball-api.herokuapp.com/api/character/");
+// Get Infos from ip
+$curl = curl_init();
 
-// Set the content type to application/json
-curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
-
-// Return instead of outputting directly
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-// Wether to include the header in the output. Set to false here
-curl_setopt($ch, CURLOPT_HEADER, 0);
-
-// 3 Execute request and fetch the response / check for error
-$results = json_decode(curl_exec($ch));
-if ($results === false) {
-    echo curl_error($ch);
+curl_setopt_array($curl, array(
+	CURLOPT_URL => "https://ip-geo-location.p.rapidapi.com/ip/37.140.128.11?format=json",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_FOLLOWLOCATION => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => array(
+		"x-rapidapi-host: ip-geo-location.p.rapidapi.com",
+		"x-rapidapi-key: 3038c2322amshbd28049ff1cb4b3p14dc77jsn0dd7720e19e9"
+	),
+));
+$response = curl_exec($curl);
+$err = curl_error($curl);
+curl_close($curl);
+if ($err) {
+	echo "cURL Error #:" . $err;
 }
 
-// 4 Close free up the curl handle
-curl_close($ch);
-
-// 5 Display result
-//print_r($results);
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>DRAGON BALL API</title>
+    <title>IP SPY</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
 
 <body>
     <div class="container">
         <div class="row">
-            <?php for ($i = 0; $i < count($results); $i++) { ?>
-                <div class="col-4">
-                    <div class="card my-4" style="width: 18rem;">
-                        <img src="<?= $results[$i]->image ?>" class="card-img-top" alt="..." style="width : 286px; height: 200px;">
-                        <div class="card-body">
-                            <h4 class="card-title"><?= $results[$i]->species ?></h4>
-                            <p class="card-text">Origin : <?= $results[$i]->originPlanet ?></p>
-                            <p class="card-text">Status : <?= $results[$i]->status ?></p>
-                            <p class="card-text">Species : <?= $results[$i]->species ?></p>
-                            <p class="card-text">Series : <?= $results[$i]->series ?></p>
-                        </div>
-                    </div>
-                </div>
-            <?php } ?>
+            <?= $response ?>
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
